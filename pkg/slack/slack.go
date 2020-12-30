85e75d928c0661765f2828e02e1d34c2ab7da488
guts <yuta1402souja@live.jp>
2020-12-30 21:14:11 +0900

    Add slack package

---
+++ pkg/slack/slack.go
@@ -0,0 +1,29 @@
+package slack
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+type requestData struct {
+	Text string `json:"text"`
+}
+
+func PostAlert(errorMessage string, apiURL string) (*http.Response, error) {
+	data := requestData{
+		Text: errorMessage,
+	}
+
+	json, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.Post(apiURL, "application/json", bytes.NewBuffer([]byte(json)))
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
